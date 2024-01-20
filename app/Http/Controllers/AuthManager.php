8c206d5d233afac836b2f73881e9a2a2d8d1c807
Musinde <musindej@gmail.com>
2024-01-20 21:15:19 +0300

    Initial login and registration

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthManager extends Controller
+{
+    function login(){
+        return view('login');
+    }
+
+    function registration(){
+        return view('registration');
+    }
+
+    function loginPost(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only(keys: 'email', 'password');
+        if(Auth::attempt($credentials)){
+            return redirect()->intended(route(name: 'home'));
+        }
+        return redirect(route(name: 'login'))->with("error", "Invalid login credentials");
+    }
+
+    function registrationPost(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+        if(!$user){
+            return redirect(route(name: 'registration'))->with("error", "Invalid registration. Try Again!");
+        }
+        return redirect(route(name: 'login'))->with("success", "Registration successful. Login to access the app");
+    }
+
+    function logout(){
+        Session::flush();
+        Auth::logout();
+        return redirect(route(name: 'login'));
+    }
+}
